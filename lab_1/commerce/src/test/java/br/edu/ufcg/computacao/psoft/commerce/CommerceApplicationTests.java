f587f7a21e16af66f1f7931e53eccb5dad01cd73
Eduardo Augusto Andrade Bezerra Cavalcanti <96199797+eduardoaugustoabc@users.noreply.github.com>
2024-05-15 17:33:14 -0300

    Add files via upload

---
+++ lab_1/commerce/src/test/java/br/edu/ufcg/computacao/psoft/commerce/CommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufcg.computacao.psoft.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
